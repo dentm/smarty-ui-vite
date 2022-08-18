c06d961a6fc946472e8613cacde4ce2dde4fe3b7
liuwenjun <15838995173@163.com>
2022-08-18 13:32:12 +0800

    refactor: 修改项目结构

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,39 @@
 <template>
-    <div mt-5>
+    <div style="margin-top: 20px;">
         <CButton color="blue">主要按钮</CButton>
         <CButton color="green">绿色按钮</CButton>
         <CButton color="gray">灰色按钮</CButton>
         <CButton color="yellow">黄色按钮</CButton>
         <CButton color="red">红色按钮</CButton>
     </div>
-    <div mt-5>
+    <div style="margin-top: 20px;">
         <CButton color="blue" plain>朴素按钮</CButton>
         <CButton color="green" plain>绿色按钮</CButton>
         <CButton color="gray" plain>灰色按钮</CButton>
         <CButton color="yellow" plain>黄色按钮</CButton>
         <CButton color="red" plain>红色按钮</CButton>
     </div>
-    <div mt-5>
+    <div style="margin-top: 20px;">
         <CButton size="small" plain>小按钮</CButton>
         <CButton size="medium" plain>中按钮</CButton>
         <CButton size="large" plain>大按钮</CButton>
     </div>
-    <div mt-5>
+    <div style="margin-top: 20px;">
         <CButton color="blue" round plain icon="search">搜索按钮</CButton>
         <CButton color="green" round plain icon="edit">编辑按钮</CButton>
         <CButton color="red" round plain icon="delete">删除按钮</CButton>
     </div>
-    <div mt-5>
+    <div style="margin-top: 20px;">
         <CButton color="blue" round plain icon="search"></CButton>
         <CButton color="green" round plain icon="edit"></CButton>
         <CButton color="red" round plain icon="delete"></CButton>
         <CButton color="red" round plain>custom</CButton>
     </div>
-
 </template>
 
 <script lang="ts" setup>
+// import '../dist/assets/entry.2022ea27.css';
+// import SmartyUI, { CButton, SFCButton, JSXButton } from '../dist/smart-ui.es'
 import CButton from './button/index';
 
 const color = 'orange'
